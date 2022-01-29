3df9807e11614cb986ab3a972bca8f2285493db9
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2022-01-29 14:36:09 +0300

    add websocket

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import EventSourcing from './EventSourcing';
+import WebSock from './WebSock';
 
 function App() {
   return (
     <div>
-      <EventSourcing />
+      <WebSock />
     </div>
   );
 }
